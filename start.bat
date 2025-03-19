23cb8d45fbe26fda9267b28be3cd98a22881d9fd
Suat Martin <sgiraygonzal@umass.edu>
2025-03-19 14:16:59 -0400

    keeping frontend

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd flutter_questions_backend
-start cmd /k "node index.js"
-start cmd /k "node captcha.js"
-timeout /t 3 /nobreak >nul
-cd ../flutter_questions
-call flutter clean
-call flutter pub get
-start "" flutter run -d chrome --web-port=59792 --web-browser-flag="--disable-cache"
